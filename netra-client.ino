fc83d3dc69518ff5bfcaf22ffcb52fe3e723ed13
Rafli <raflisatriandi@outlook.co.id>
2023-04-03 13:52:41 +0700

    writelcd from serial done

--- netra-client.ino
+++ netra-client.ino
@@ -2,52 +2,15 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// main section
 String cmd = "";
-String frstLine = "";
-String scndLine = "";
 int currIndex = 0;
 
-int end = 13, start = 0;
-
-void writeLcd(String text);
-void writeLcdFull(String text);
-void movingText();
-
-void setup() {
-  Serial.begin(9600);
-  while (!Serial) {
-    ;
-  }
-  Serial.println("\nSerial connected!");
-  Serial.println("Initialize LCD...");
-  pinMode(2, OUTPUT);
-  digitalWrite(2, HIGH);
-  lcd.init();
-  lcd.backlight();
-  lcd.clear();
-  Serial.println("LCD Initialized!");
-}
-
-int id = 13;
-
-void loop() {
-  // movingText();
-  if (Serial.available() > 0) {
-    cmd = Serial.readString();
-    cmd.trim();
-    if (cmd.length() > 16) {
-      cmd.remove(13);
-      cmd += "...";
-    }
-    Serial.print("Command: ");
-    Serial.println(cmd);
-    writeLcdFull(cmd);
-    // frstLine = cmd;
-  }
-}
-
 void writeLcd(String text) {
+  String in_txt = text;
+  if (in_txt.length() > 16) {
+    in_txt.remove(13);
+    in_txt += "...";
+  }
   lcd.setCursor(0, currIndex);
   if (currIndex == 0) {
     lcd.clear();
@@ -55,34 +18,38 @@ void writeLcd(String text) {
   } else {
     currIndex--;
   }
-  lcd.print(text);
+  lcd.print(in_txt);
 }
 
-void writeLcdFull(String text) {
-  if (currIndex == 0) {
-    frstLine = text;
-  } else {
-    scndLine = text;
+void cmdProcessor(String cmd){
+  if(cmd == "STATUS"){
+    Serial.println("UP");
+    return;
   }
-  writeLcd(text);
+  if(cmd.indexOf("SEND ") == 0){
+    cmd.remove(0, 5);
+    writeLcd(cmd);
+  }
+  return;
 }
 
-void movingText() {
-  String temp = frstLine;
-  if (end == frstLine.length()) {
-    end = 13;
-    start = 0;
+void setup() {
+  // pinMode(6, INPUT);
+  // pinMode(13, OUTPUT);
+  Serial.begin(19200);
+  while (!Serial) {
+    ;
   }
-  if (frstLine.length() > 16) {
-    temp.trim();
-    if (temp.length() > 16) {
-      temp.remove(end);
-      temp += "...";
-    }
+  Serial.println("UP");
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+}
+
+void loop() {
+  if (Serial.availableForWrite() > 0) {
+    cmd = Serial.readString();
+    cmd.trim();
+    cmdProcessor(cmd);
   }
-  lcd.setCursor(0, 0);
-  lcd.print(temp);
-  delay(500);
-  end++;
-  start++;
 }
